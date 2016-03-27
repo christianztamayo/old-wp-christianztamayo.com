16263f5b2635df42e50177093ed91dfd0281a3fe
Christian Z. Tamayo <christianztamayo@gmail.com>
2016-03-28 02:15:25 +0800

    Grid and favicon improvements

--- web/app/themes/czt/base.php
+++ web/app/themes/czt/base.php
@@ -3,9 +3,7 @@
 use Roots\Sage\Setup;
 use Roots\Sage\Wrapper;
 
-?>
-
-<!doctype html>
+?><!doctype html>
 <html <?php language_attributes(); ?>>
   <?php get_template_part('templates/head'); ?>
   <body <?php body_class(); ?>>
@@ -14,12 +12,12 @@ use Roots\Sage\Wrapper;
         <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
       </div>
     <![endif]-->
-    <div class="wrap" role="document">
+    <div class="wrap">
       <?php
         do_action('get_header');
         get_template_part('templates/header');
       ?>
-      <div class="content">
+      <div class="content" role="document">
         <main class="content-main" role="main">
           <?php if (Setup\has_wrap()) : ?><div class="container-fluid"><?php endif; ?>
             <?php include Wrapper\template_path(); ?>
@@ -30,6 +28,7 @@ use Roots\Sage\Wrapper;
             <?php include Wrapper\sidebar_path(); ?>
           </aside>
         <?php endif; ?>
+      </div>
       <?php
         do_action('get_footer');
         get_template_part('templates/footer');
